19f117e7b5033dda0f7c80a9f5466aeb25430177
rmsepskek02 <rmsepskek02@gmail.com>
2024-09-10 17:59:45 +0900

    1
    1

--- Assets/MyDefenseStudy/Scripts/TurretBlueprint.cs
+++ Assets/MyDefenseStudy/Scripts/TurretBlueprint.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class TurretBlueprint : MonoBehaviour
 {
+    // 공개 필드는 기본적으로 직렬화됩니다.
+    public string playerName;
+    public int health;
+    public float speed;
+
+    // 비공개 필드도 [SerializeField] 어트리뷰트를 사용하면 직렬화됩니다.
+    [SerializeField]
+    private int armor;
+
+    // 프로퍼티는 직렬화되지 않습니다. 필요 시 커스텀 에디터를 사용해야 합니다.
+    public int Armor
+    {
+        get { return armor; }
+        set { armor = value; }
+    }
     // Start is called before the first frame update
     void Start()
     {
